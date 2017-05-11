9a1c9f9fee5770e41ea8875aead712a97a763d41
DiogoCosta98 <diansico98@outlook.pt>
2017-05-11 03:12:53 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 #include "bikes.h"
+#include "argtest.h"
 
 int main(int argc, char*argv[])
 {
+    arguments_test(argc,argv);
     return EXIT_SUCCESS;
 }
