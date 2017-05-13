350852c842f13451bbf99a174a473a7a7268a559
DiogoCosta98 <diansico98@outlook.pt>
2017-05-13 23:56:49 +0100

    Update main.c

--- main.c
+++ main.c
@@ -10,11 +10,15 @@
 int main(int argc, char*argv[])
 {
     arguments_test(argc, argv);
+    
+    ///lists are created and the pointers to the last items are assigned to triptail and stationtail
+    triptail=tripfile_read();
+    stationtail=stationfile_read();
+    
+    printf("%d %d", triptail->prev->trip_file.timespan, stationtail->prev->station_file.stationID); //para testar
 
     if(strcmp(argv[1], "-t")==0)
         textmode();
 
     return EXIT_SUCCESS;
 }
-
-
