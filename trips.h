8bc99480c1202acfe7f79adb0672115603923836
DiogoCosta98 <diansico98@outlook.pt>
2017-05-14 00:06:53 +0100

    Update trips.h

--- trips.h
+++ trips.h
@@ -1,35 +1,58 @@
-#ifndef STATIONS_H_INCLUDED
-#define STATIONS_H_INCLUDED
+#ifndef TRIPS_H_INCLUDED
+#define TRIPS_H_INCLUDED
+#define BUFSIZE 100
+#include "stations.h"
 
 typedef struct
 {
-    int stationID;
-    char terminal[7];
-    char station[250];
-    char municipal[10];
-    double lat;
-    double lng;
-    char status[10];
-} station_data;
+    int hour;
+    int minute;
+} ttime;
 
-typedef struct station
+
+typedef struct
 {
-    station_data station_file;
-    struct station *prev;
-    struct station *next;
-} stationnode;
-
-stationnode* stationtail;
-
-void stationlisting(void);
-void routelisting(void);
-
-stationnode* stationfile_read(void);
-stationnode* create_stationlist(station_data,stationnode*);
-stationnode* stationnode_alloc(stationnode*,stationnode*);
+    int day;
+    int month;
+    int year;
+} ddate;
 
 
-#endif // STATIONS_H_INCLUDED
+typedef struct
+{
+    char type[10];
+    int birthyear;
+    char gender[6];
+} person;
+
+
+
+typedef struct
+{
+    int tripID;
+    int timespan;
+    ttime timebegin;
+    ttime timeend;
+    ddate datebegin;
+    ddate dateend;
+    char bikeID[7];
+    person user;
+    int startstationID;
+    int stopstationID;
+    station_data *start;
+    station_data *stop;
+
+} trip_data;
+
+typedef struct trip
+{
+    trip_data trip_file;
+    struct trip *prev;
+    struct trip *next;
+} tripnode;
+
+
+tripnode* triptail;
 
 void textmode(void);
 void dataselection(int*,ttime*, ttime*, int*, int*);
@@ -39,7 +62,12 @@ void criterion1(ttime*, ttime*);
 int criterion2(void);
 int criterion3(void);
 void ClearData(ttime*, ttime*, int*, int*);
-void triplisting(void);
 void statisticslisting(void);
+void triplisting(void);
+
+tripnode* tripfile_read(void);
+tripnode* create_triplist(trip_data,tripnode*);
+tripnode* tripnode_alloc(tripnode*,tripnode*);
+
 
 #endif // TRIPS_H_INCLUDED
