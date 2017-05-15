032b0049fc70c9d0bff0989eb95aa3a7da4b0c19
DiogoCosta98 <diansico98@outlook.pt>
2017-05-15 14:24:12 +0100

    Update listfill.c

--- listfill.c
+++ listfill.c
@@ -8,12 +8,11 @@
  *  Reads values from trip file and saves them in a list of structures.
  *  Returns a pointer do the list tail
  */
-tripnode* tripfile_read()
+void tripfile_read()
 {
     char string1[BUFSIZE], string2[BUFSIZE], string3[BUFSIZE];
     int testread, seconds;
     trip_data tripread;
-    tripnode* prev_node=NULL;
     FILE *tripsfptr;
     tripsfptr=fopen( "hubway_trips_v2.csv", "r");
     if (tripsfptr)
@@ -23,32 +22,31 @@ tripnode* tripfile_read()
             testread=fscanf(tripsfptr,"%s %s %s",string1,string2,string3);
             if(testread==3)
             {
-                sscanf(string1,"%d,%d,%d/%d/%d", &tripread.tripID, &tripread.timespan, &tripread.datebegin.day,
-                       &tripread.datebegin.month, &tripread.datebegin.year);
+                sscanf(string1,"%d,%d,%d/%d/%d", &tripread.tripID, &tripread.timespan, &tripread.datebegin.month,
+                       &tripread.datebegin.day, &tripread.datebegin.year);
 
                 sscanf(string2,"%d:%d:%d,%d,%d/%d/%d", &tripread.timebegin.hour, &tripread.timebegin.minute, &seconds,
-                       &tripread.startstationID,&tripread.dateend.day,&tripread.dateend.month,&tripread.dateend.year);
+                       &tripread.startstationID,&tripread.dateend.month,&tripread.dateend.day,&tripread.dateend.year);
 
                 sscanf(string3,"%d:%d:%d,%d,%[^,],%[^,],%d,%s", &tripread.timeend.hour, &tripread.timeend.minute, &seconds,
                        &tripread.stopstationID, tripread.bikeID, tripread.user.type, &tripread.user.birthyear, tripread.user.gender);
 
-                prev_node=create_triplist(tripread,prev_node);
+               create_triplist(tripread);
             }
         }
         while(testread==3);
         fclose(tripsfptr);
     }
-    return prev_node;
+    return;
 }
 
 /**
  *  Reads station from stations file and saves them in a structure and returns it
  */
-stationnode* stationfile_read()
+void stationfile_read()
 {
     int testread;
     station_data stationread;
-    stationnode* prev_node=NULL;
 
     FILE *stationsfptr;
     stationsfptr=fopen( "hubway_stations.csv", "r");
@@ -60,44 +58,38 @@ stationnode* stationfile_read()
                             stationread.municipal, &stationread.lat, &stationread.lng, stationread.status);
             if(testread==7)
             {
-                prev_node=create_stationlist(stationread,prev_node);
+                create_stationlist(stationread);
             }
 
         }
         while(testread==7);
         fclose(stationsfptr);
     }
-    return prev_node;
+    return;
 }
 
 /**
  *  Fills each trip structure station start and stop pointer with the pointers that match the correspondent structures
  */
-void fillstation_triplist(tripnode* lasttrip,stationnode* laststation)
+void fillstation_triplist()
 {
-    tripnode* curtrip;
-    stationnode* curstation;
-    int startstation_done, stopstation_done;
+    tripnode* temptrip=triphead;
+    stationnode* tempstation=stationhead;
 
-    ///goes through the trip list from tail to head
-    for(curtrip=lasttrip; curtrip!=NULL; curtrip=curtrip->prev)
-    {
-        startstation_done=stopstation_done=0;
-        ///goes through the station list from tail to head until both start and stop stations are found
-        for(curstation=laststation; curstation!=NULL && (startstation_done!=1  || stopstation_done!=1); curstation=curstation->prev)
+    while(temptrip!=NULL){
+        while(tempstation!=NULL)
         {
-            ///if the current station has the same ID as the starting one a pointer to its structure is assigned
-            if(curstation->station_file.stationID==curtrip->trip_file.startstationID)
+            if(temptrip->trip_file.startstationID==tempstation->station_file.stationID)
             {
-                curtrip->trip_file.start=&(curstation->station_file);
-                startstation_done=1;
+                temptrip->trip_file.start=&(tempstation->station_file);
             }
-            ///if the current station has the same ID as the stopping one a pointer to its structure is assigned
-            if(curstation->station_file.stationID==curtrip->trip_file.stopstationID)
+            if(temptrip->trip_file.stopstationID==tempstation->station_file.stationID)
             {
-                curtrip->trip_file.stop=&(curstation->station_file);
-                stopstation_done=1;
+                temptrip->trip_file.stop=&(tempstation->station_file);
             }
+            tempstation=tempstation->next;
         }
+        temptrip=temptrip->next;
+        tempstation=stationhead;
     }
 }
