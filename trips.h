78e67f71cdac3726e67cc7292c1c575a41e10c8e
DiogoCosta98 <diansico98@outlook.pt>
2017-05-16 18:44:16 +0100

    Update trips.h

--- trips.h
+++ trips.h
@@ -56,6 +56,7 @@ void criterion1(ttime*, ttime*, tripnode**);
 int criterion2(tripnode**);
 int criterion3(tripnode**);
 void ClearData(char**, tripnode**, stationnode**);
+void routelisting(tripnode**,stationnode**);
 void statisticslisting(void);
 void triplisting(tripnode**);
 void load_fromfiles(char**, tripnode**, stationnode**);
@@ -65,6 +66,78 @@ void InsertTripList(tripnode**, trip_data);
 tripnode* RemoveUsingHour(ttime*, ttime*, tripnode*);
 tripnode* RemoveUsingWeekday(int, tripnode*);
 tripnode* RemoveUsingMaxduration(int, tripnode*);
+void RemoveNode(tripnode**,tripnode**, tripnode**);
+int ConvertDate(int, int, int);
+int LeapYear(int);
+
+#endif // TRIPS_H_INCLUDED#ifndef TRIPS_H_INCLUDED
+#define TRIPS_H_INCLUDED
+#define BUFSIZE 1000
+#include "stations.h"
+
+typedef struct
+{
+    int hour;
+    int minute;
+} ttime;
+
+
+typedef struct
+{
+    int day;
+    int month;
+    int year;
+} ddate;
+
+
+typedef struct
+{
+    int type;
+    int birthyear;
+    int gender;
+} person;
+
+
+typedef struct
+{
+    int tripID;
+    int timespan;
+    ttime timebegin;
+    ttime timeend;
+    ddate datebegin;
+    ddate dateend;
+    char bikeID[7];
+    person user;
+    int stationstartID;
+    int stationstopID;
+
+} trip_data;
+
+typedef struct trip
+{
+    trip_data trip_file;
+    struct trip *next;
+} tripnode;
+
+
+void textmode(char**, tripnode**, stationnode**);
+void dataselection(int*,ttime*, ttime*, int*, int*, tripnode**, stationnode**, char**);
+void criterion(ttime*, ttime*, int*, int*, tripnode**, stationnode**, char**);
+int testchoice(int);
+void criterion1(ttime*, ttime*, tripnode**);
+int criterion2(tripnode**);
+int criterion3(tripnode**);
+void ClearData(char**, tripnode**, stationnode**);
+void statisticslisting(void);
+void triplisting(tripnode**);
+void load_fromfiles(char**, tripnode**, stationnode**);
+void load_tripfile(tripnode**, FILE*);
+tripnode* NewTripNode(trip_data);
+void InsertTripList(tripnode**, trip_data);
+tripnode* RemoveUsingHour(ttime*, ttime*, tripnode*);
+tripnode* RemoveUsingWeekday(int, tripnode*);
+tripnode* RemoveUsingMaxduration(int, tripnode*);
+void RemoveNode(tripnode**,tripnode**, tripnode**);
 int ConvertDate(int, int, int);
 int LeapYear(int);
 
